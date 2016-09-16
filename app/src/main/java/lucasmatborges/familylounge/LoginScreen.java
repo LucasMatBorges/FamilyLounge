8371e6b94142ea3919841baa05c052cbeb651e6c
Lucas Matheus Borges <lucasmatborges@gmail.com>
2016-09-16 08:52:30 -0300

    Arrumando o Login Screen (Delay)

--- app/src/main/java/lucasmatborges/familylounge/LoginScreen.java
+++ app/src/main/java/lucasmatborges/familylounge/LoginScreen.java
@@ -1,6 +1,7 @@
 package lucasmatborges.familylounge;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -83,8 +84,8 @@ public class LoginScreen extends AppCompatActivity {
                 String[] arrayWords = Prontuario.split(",");
                 int n = arrayWords.length; // Gives n such that 0 <= n < 2 (arrayWords.length)
                 for (int i = 0; i < arrayWords.length; i++) {
-                    System.out.println("Verifica: " + prontuario);
-                    System.out.println("Verifica: " + arrayWords[i]);
+                    System.out.println("For prontuario: " + prontuario);
+                    System.out.println("For array: " + arrayWords[i]);
 
                     if (prontuario.equals(arrayWords[i])) { // char é uma letra
                         textoverifica.setText("true");
@@ -98,21 +99,41 @@ public class LoginScreen extends AppCompatActivity {
             }
         });
 
-        String verifica = textoverifica.getText().toString();
-        System.out.println("Verifica: " + verifica);
-        System.out.println("Colocada: " + prontuario);
+        // Comanda para criar a splashScreen
+        Runnable runnable3secs = new Runnable() {
+            @Override
+            public void run() {
+                verificar();
 
-        if (verifica.equals("true")){ // NÃO FUNCIONA O "=="
-            editText.setText("");
-            Intent openGameMulti = new Intent(this, MainActivity.class);
-            openGameMulti.putExtra("POINTS_IDENTIFIER", prontuario);
-            startActivity(openGameMulti);
-            Toast.makeText(this, verifica, Toast.LENGTH_SHORT).show();
-            System.out.println("Verifica: " + verifica);}
-        else {
-            Toast.makeText(this, "Não achamos esse prontuário", Toast.LENGTH_SHORT).show();
             }
-        }
+        };
+
+        Handler myhandler = new Handler();
+        myhandler.postDelayed(runnable3secs,500); // 3000 = tempo de execução da splash ( 3000 milisegundos = 3 segundos)
+
     }
+
+
+public void verificar (){
+    EditText editText = (EditText) findViewById(R.id.editTextID);
+    final String prontuario = editText.getText().toString();
+    final TextView textoverifica = (TextView) findViewById(R.id.textViewProntuario);
+    String verifica = textoverifica.getText().toString();
+    System.out.println("Dps da conversão Verifica: " + verifica);
+    System.out.println("Colocada: " + prontuario);
+
+    if (verifica.equals("true")){ // NÃO FUNCIONA O "=="
+        editText.setText("");
+        Intent openGameMulti = new Intent(this, MainActivity.class);
+        openGameMulti.putExtra("POINTS_IDENTIFIER", prontuario);
+        startActivity(openGameMulti);
+        Toast.makeText(this, verifica, Toast.LENGTH_SHORT).show();
+        System.out.println("true: " + verifica);}
+    else {
+        Toast.makeText(this, "Não achamos esse prontuário", Toast.LENGTH_SHORT).show();
+        System.out.println("True ou false: " + verifica);
+
+    }
+}}
 
 
